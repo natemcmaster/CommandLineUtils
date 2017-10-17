fd56f3016452bb1c55e16012019f1e1e768e526c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-10-17 18:04:29 +0200

    Merge pull request #7 - Fix for CommandLineApplication Option Extensions
    * Added VerboseOption overload to allow template by argument
    * Fallback to AssemblyName version if no InformationalVersion is applied for the assembly

--- src/CommandLineUtils/CommandLineApplicationExtensions.cs
+++ src/CommandLineUtils/CommandLineApplicationExtensions.cs
@@ -26,7 +26,16 @@ namespace McMaster.Extensions.CommandLineUtils
         /// <param name="app"></param>
         /// <returns></returns>
         public static CommandOption VerboseOption(this CommandLineApplication app)
-            => app.Option("-v|--verbose", "Show verbose output", CommandOptionType.NoValue, inherited: true);
+            => VerboseOption(app, "-v|--verbose");
+
+        /// <summary>
+        /// Adds the verbose option with the template <c>-v|--verbose</c>.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="template" />
+        /// <returns></returns>
+        public static CommandOption VerboseOption(this CommandLineApplication app, string template)
+            => app.Option(template, "Show verbose output", CommandOptionType.NoValue, inherited: true);
 
         /// <summary>
         /// Sets <see cref="CommandLineApplication.Invoke"/> with a return code of <c>0</c>.
@@ -43,21 +52,40 @@ namespace McMaster.Extensions.CommandLineUtils
         /// <summary>
         /// Finds <see cref="AssemblyInformationalVersionAttribute"/> on <paramref name="assembly"/> and uses that
         /// to set <see cref="CommandLineApplication.OptionVersion"/>.
+        /// <para>
+        /// Uses the Version that is part of the <see cref="AssemblyName"/> of the specified assembly
+        /// if no <see cref="AssemblyInformationalVersionAttribute"/> is applied.
+        /// </para>
         /// </summary>
         /// <param name="app"></param>
         /// <param name="assembly"></param>
-        public static void VersionOptionFromAssemblyAttributes(this CommandLineApplication app, Assembly assembly)
-            => app.VersionOption("--version", GetInformationalVersion(assembly));
+        /// <exception cref="ArgumentNullException">Either <paramref name="app"/> or <paramref name="assembly"/> is <c>null</c>.</exception>
+        public static CommandOption VersionOptionFromAssemblyAttributes(this CommandLineApplication app, Assembly assembly)
+            => VersionOptionFromAssemblyAttributes(app, "--version", assembly);
+
+        /// <summary>
+        /// Finds <see cref="AssemblyInformationalVersionAttribute"/> on <paramref name="assembly"/> and uses that
+        /// to set <see cref="CommandLineApplication.OptionVersion"/>.
+        /// <para>
+        /// Uses the Version that is part of the <see cref="AssemblyName"/> of the specified assembly
+        /// if no <see cref="AssemblyInformationalVersionAttribute"/> is applied.
+        /// </para>
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="template"></param>
+        /// <param name="assembly"></param>
+        /// <exception cref="ArgumentNullException">Either <paramref name="app"/> or <paramref name="assembly"/> is <c>null</c>.</exception>
+        public static CommandOption VersionOptionFromAssemblyAttributes(CommandLineApplication app, string template, Assembly assembly)
+            => app.VersionOption(template, GetInformationalVersion(assembly));
 
         private static string GetInformationalVersion(Assembly assembly)
         {
-            var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
-
-            var versionAttribute = attribute == null
-                ? assembly.GetName().Version.ToString()
-                : attribute.InformationalVersion;
-
-            return versionAttribute;
+            string infoVersion = assembly?
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion;
+            if (string.IsNullOrWhiteSpace(infoVersion))
+                return assembly?.GetName().Version.ToString();
+            return infoVersion;
         }
     }
 }
