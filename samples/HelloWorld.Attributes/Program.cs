242eff445abde5fb4e30b4d3b7b7bc8bc7c63fcb
Nate McMaster <nate.mcmaster@gmail.com>
2017-10-25 22:44:40 -0700

    Add a simple sample for using attributes

---
+++ samples/HelloWorld.Attributes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace HelloWorld
+{
+    class CommandLineOptions
+    {
+        [Option]
+        public string Subject { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = CommandLineApplication.ParseArgs<CommandLineOptions>(args);
+            var subject = options.Subject ?? "world";
+            Console.WriteLine($"Hello {subject}!");
+        }
+    }
+}
