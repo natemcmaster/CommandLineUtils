7d3d726513f3f9659184a4bfb11f070a05f91309
Nate McMaster <nate.mcmaster@gmail.com>
2017-11-10 19:46:38 -0800

    Upgrade xunit and test dependencies and fix compilation error

--- test/CommandLineUtils.Tests/ReflectionAppBuilderTests.cs
+++ test/CommandLineUtils.Tests/ReflectionAppBuilderTests.cs
@@ -211,6 +211,118 @@ namespace McMaster.Extensions.CommandLineUtils.Tests
                 ex.Message);
         }
 
+        private class SimpleProgram
+        {
+            [Argument(0)]
+            public string Command { get; set; }
+
+            [Option]
+            public string Message { get; set; }
+
+            [Option("-F <file>")]
+            public string File { get; set; }
+
+            [Option]
+            public bool Amend { get; set; }
+
+            [Option("--no-edit")]
+            public bool NoEdit { get; set; }
+        }
+
+        [Fact]
+        public void InitializesTypeWithAttributes()
+        {
+            var program = CommandLineParser.ParseArgs<SimpleProgram>("commit", "-m", "Add attribute parsing", "--amend");
+
+            Assert.NotNull(program);
+            Assert.Equal("commit", program.Command);
+            Assert.Equal("Add attribute parsing", program.Message);
+            Assert.True(program.Amend);
+            Assert.False(program.NoEdit);
+        }
+
+        [Fact]
+        public void ThrowsForArgumentsWithoutMatchingAttribute()
+        {
+            var ex = Assert.Throws<CommandParsingException>(
+                () => CommandLineParser.ParseArgs<SimpleProgram>("-f"));
+            Assert.StartsWith("Unrecognized option '-f'", ex.Message);
+        }
+
+        private class PrivateSetterProgram
+        {
+            public int _value;
+            public static int _staticvalue;
+
+            [Option]
+            public int Number { get; private set; }
+
+            [Option]
+            public int Count { get; }
+
+            [Option]
+            public int Value
+            {
+                get => _value;
+                set => _value = value + 1;
+            }
+
+            [Option("--static-number")]
+            public static int StaticNumber { get; private set; }
+
+            [Option("--static-string")]
+            public static string StaticString { get; }
+
+            [Option("--static-value")]
+            public static int StaticValue
+            {
+                get => _staticvalue;
+                set => _staticvalue = value + 1;
+            }
+        }
+
+        [Fact]
+        public void BindsToPrivateSetProperties()
+        {
+            var parsed = CommandLineParser.ParseArgs<PrivateSetterProgram>("--number", "1");
+            Assert.Equal(1, parsed.Number);
+        }
+
+        [Fact]
+        public void BindsToReadOnlyProperties()
+        {
+            var parsed = CommandLineParser.ParseArgs<PrivateSetterProgram>("--count", "1");
+            Assert.Equal(1, parsed.Count);
+        }
+
+        [Fact]
+        public void BindsToPropertiesWithSetterMethod()
+        {
+            var parsed = CommandLineParser.ParseArgs<PrivateSetterProgram>("--value", "1");
+            Assert.Equal(2, parsed.Value);
+        }
+
+        [Fact]
+        public void BindsToStaticProperties()
+        {
+            CommandLineParser.ParseArgs<PrivateSetterProgram>("--static-number", "1");
+            Assert.Equal(1, PrivateSetterProgram.StaticNumber);
+        }
+
+        [Fact]
+        public void BindsToStaticReadOnlyProps()
+        {
+            CommandLineParser.ParseArgs<PrivateSetterProgram>("--static-string", "1");
+            Assert.Equal("1", PrivateSetterProgram.StaticString);
+        }
+
+        [Fact]
+        public void BindsToStaticPropertiesWithSetterMethod()
+        {
+            CommandLineParser.ParseArgs<PrivateSetterProgram>("--static-value", "1");
+            Assert.Equal(2, PrivateSetterProgram.StaticValue);
+        }
+
         [Theory]
         [InlineData("Option123", "o", "option123")]
         [InlineData("dWORD", "d", "d-word")]
---
+++ test/CommandLineUtils.Tests/Utilities/CommandLineParser.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Nate McMaster.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace McMaster.Extensions.CommandLineUtils
+{
+    /// <summary>
+    /// Utilities for parsing a command line application
+    /// </summary>
+    internal static class CommandLineParser
+    {
+        public static T ParseArgs<T>(params string[] args)
+            where T : class, new()
+        {
+            var applicationBuilder = new ReflectionAppBuilder<T>();
+            return (T)applicationBuilder.Bind(NullConsole.Singleton, args).Target;
+        }
+    }
+}
