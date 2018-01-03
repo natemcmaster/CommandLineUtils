5849b59f369f36a863eecefbce0aa731c394d944
Nate McMaster <nate.mcmaster@gmail.com>
2018-01-03 08:32:11 -0800

    Make subcommand sample more interactive - #36

--- samples/Subcommands/Program.cs
+++ samples/Subcommands/Program.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.ComponentModel.DataAnnotations;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace SubcommandSample
@@ -10,25 +11,42 @@ namespace SubcommandSample
     /// This example is meant to show you how to structure a console application that uses
     /// the nested subcommands with options and arguments that vary between each subcommand.
     /// </summary>
+    [Command(ThrowOnUnexpectedArgument = false)]
     class Program
     {
-        public static int Main(string[] args)
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Argument(0), Range(1, 3)]
+        public int? Option { get; set; }
+
+        public string[] RemainingArgs { get; set; }
+
+        private int OnExecute()
         {
             const string prompt = @"Which example would you like to run?
 1 - Fake Git
 2 - Fake Docker
 3 - Fake npm
 > ";
-            var option = Prompt.GetInt(prompt);
+            if (!Option.HasValue)
+            {
+                Option = Prompt.GetInt(prompt);
+            }
 
-            switch (option)
+            if (RemainingArgs == null || RemainingArgs.Length == 0)
+            {
+                var args = Prompt.GetString("Enter some arguments >");
+                RemainingArgs = args.Split(' ');
+            }
+
+            switch (Option)
             {
                 case 1:
-                    return CommandLineApplication.Execute<Git>(args);
+                    return CommandLineApplication.Execute<Git>(RemainingArgs);
                 case 2:
-                    return CommandLineApplication.Execute<Docker>(args);
+                    return CommandLineApplication.Execute<Docker>(RemainingArgs);
                 case 3:
-                    return Npm.Main(args);
+                    return Npm.Main(RemainingArgs);
                 default:
                     Console.Error.WriteLine("Unknown option");
                     return 1;
