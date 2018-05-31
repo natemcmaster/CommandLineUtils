f1419a08c1c81d84eef3ce2906c83678a998a44e
Nate McMaster <nate.mcmaster@gmail.com>
2018-05-30 23:56:42 -0700

    Update samples and tests to .NET Core 2.1

--- test/CommandLineUtils.Tests/DotNetExeTests.cs
+++ test/CommandLineUtils.Tests/DotNetExeTests.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // This file has been modified from the original form. See Notice.txt in the project root for more information.
 
-#if (NETCOREAPP1_1 || NETCOREAPP2_0)
+#if NETCOREAPP2_1
 using System.IO;
 using Xunit;
 
