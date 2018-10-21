51b186261d0d79d2e51ca6742d4aa570b1d6b072
Lucas Theisen <lucastheisen@pastdev.com>
2018-10-21 16:33:47 -0400

    combine UseCliLifetime and UseCli as lifetime depends on cli anyway

--- src/Hosting.CommandLine/HostBuilderExtensions.cs
+++ src/Hosting.CommandLine/HostBuilderExtensions.cs
@@ -14,7 +14,7 @@ namespace McMaster.Extensions.Hosting.CommandLine
     {
         /// <summary>
         /// Runs <code>T</code> as a <see cref="CommandLineApplication"/> with the
-        /// supplied <code>args</code>.  This method should be the primary approach 
+        /// supplied <code>args</code>.  This method should be the primary approach
         /// taken for command line applications.
         /// </summary>
         /// <typeparam name="T">The command line application implementation</typeparam>
@@ -23,7 +23,7 @@ namespace McMaster.Extensions.Hosting.CommandLine
         /// <returns>A task whose result is the exit code of the application</returns>
         public static async Task<int> RunCliAsync<T>(this IHostBuilder hostBuilder, string[] args) where T : class
         {
-            using (var host = hostBuilder.UseCliLifetime().UseCli<T>(args).Build()) 
+            using (var host = hostBuilder.UseCli<T>(args).Build())
             {
                 await host.StartAsync();
                 await host.WaitForShutdownAsync();
@@ -32,22 +32,7 @@ namespace McMaster.Extensions.Hosting.CommandLine
         }
 
         /// <summary>
-        /// Sets the <see cref="IHostLifetime"/> to one that handles running a 
-        /// <see cref="CommandLineApplication"/>.  It will ensure the
-        /// <see cref="ICliService"/> will be run to completion, the exit code gathered,
-        /// and the hosted application stopped.
-        /// <para>
-        /// This method is not intended to be used directly.  Instead, you should use
-        /// <see cref="RunCliAsync{T}(IHostBuilder, string[])"/>.
-        /// </para>
-        /// </summary>
-        /// <param name="hostBuilder">This instance</param>
-        /// <returns>This instance</returns>
-        public static IHostBuilder UseCliLifetime(this IHostBuilder hostBuilder)
-            => hostBuilder.ConfigureServices((context, services) => services.AddSingleton<IHostLifetime, CliLifetime>());
-
-        /// <summary>
-        /// Sets the <see cref="ICliService"/> to use <code>T</code> for its 
+        /// Sets the <see cref="ICliService"/> to use <code>T</code> for its
         /// <see cref="CommandLineApplication"/>.
         /// <para>
         /// This method is not intended to be used directly.  Instead, you should use
@@ -60,10 +45,9 @@ namespace McMaster.Extensions.Hosting.CommandLine
         /// <returns>This instance</returns>
         public static IHostBuilder UseCli<T>(this IHostBuilder hostBuilder, string[] args) where T : class
             => hostBuilder.ConfigureServices(
-                    (context, services) =>
-                    {
-                        services.AddSingleton<CliArgs>(new CliArgs{Value = args});
-                        services.AddSingleton<ICliService, CliService<T>>();
-                    });
+                (context, services)
+                    => services.AddSingleton<IHostLifetime, CliLifetime>()
+                        .AddSingleton<CliArgs>(new CliArgs{Value = args})
+                        .AddSingleton<ICliService, CliService<T>>());
     }
 }
