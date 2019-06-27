afd400f366eb2e9ffea1f9c9cb1a7ddcbd97b3f0
Kyle Rader <kyrader@microsoft.com>
2019-06-27 14:05:43 -0700

    Implement DescriptionFormatter and add more test caases

--- src/CommandLineUtils/HelpText/DescriptionFormatter.cs
+++ src/CommandLineUtils/HelpText/DescriptionFormatter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace McMaster.Extensions.CommandLineUtils.HelpText
 {
@@ -8,20 +10,65 @@ namespace McMaster.Extensions.CommandLineUtils.HelpText
     /// </summary>
     internal class DescriptionFormatter
     {
-        private int _firstColumnWidth;
-        private int _spacerLength;
-        private int _consoleWidth;
+        /// <summary>
+        /// The length of the first column width plus the column spacer length.
+        /// </summary>
+        private int _paddedLineLength;
 
+        private int _consoleWidth;
+        private string _paddedLine;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstColumnWidth">The width of the first column where the option/argument/command is listed.</param>
+        /// <param name="spacerLength">The number of spaces separating the columns.</param>
+        /// <param name="consoleWidth">The max width of the console controlling when to wrap to a new line.
+        /// Defaults to Console.BufferWidth.
+        /// </param>
         public DescriptionFormatter(int firstColumnWidth, int spacerLength, int? consoleWidth = null)
         {
-            _firstColumnWidth = firstColumnWidth;
-            _spacerLength = spacerLength;
+            _paddedLineLength = firstColumnWidth + spacerLength;
             _consoleWidth = consoleWidth ?? Console.BufferWidth;
+            _paddedLine = "\n" + new string(' ', _paddedLineLength);
         }
 
         public string Wrap(string original)
         {
-            return "";
+            var lines = original
+                .Split(new[] { "\n", "\r\n" }, StringSplitOptions.None)
+                .Select(WrapSingle);
+
+            return string.Join(_paddedLine, lines);
+        }
+
+        public string WrapSingle(string original)
+        {
+            StringBuilder sb = new StringBuilder();
+            var lineLength = _paddedLineLength;
+            foreach (var token in original.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (lineLength == _paddedLineLength)
+                {
+                    // At the beginning of a new padded line, just append token.
+                }
+                else if (lineLength + 1 + token.Length > _consoleWidth)
+                {
+                    // Adding a space + token would push over console width, need a new line.
+                    sb.Append(_paddedLine);
+                    lineLength = _paddedLineLength;
+                }
+                else
+                {
+                    // We can add a space and the token so add the space.
+                    sb.Append(' ');
+                    lineLength++;
+                }
+
+                sb.Append(token);
+                lineLength += token.Length;
+            }
+            return sb.ToString();
         }
     }
 }
--- test/CommandLineUtils.Tests/DescriptionFormatterTests.cs
+++ test/CommandLineUtils.Tests/DescriptionFormatterTests.cs
@@ -6,26 +6,80 @@ namespace McMaster.Extensions.CommandLineUtils.Tests
 {
     public class DescriptionFormatterTests
     {
+        private const int FirstColumnWidth = 18;
+        private const int SpacerLength = 2;
+        private const int ConsoleWidth = 40; // A very skinny console for testing.
+
+        // Always using \n in our wrapped output as Consoles in Windows these days are fine with this.
+        // As of RS5 so is notepad.exe on Windows.
+        private string _paddedNewline = $"\n{new string(' ', FirstColumnWidth + SpacerLength)}";
+
+        private DescriptionFormatter Subject()
+        {
+            return new DescriptionFormatter(FirstColumnWidth, SpacerLength, ConsoleWidth);
+        }
+
+        private void AssertExpected(string original, string expected)
+        {
+            Subject().Wrap(original).Should().Be(expected);
+        }
+
+        [Fact]
+        public void SimpleOneLineJustWorks()
+        {
+            var originalText = "Verbosity setting";
+            var expected = "Verbosity setting";
+            AssertExpected(originalText, expected);
+        }
+
         [Fact]
         public void ItWrapsADescriptionBasedOnConsoleWidthAndFirstColumnSize()
         {
             var originalText = "This argument description is really long. It is a great argument. The best argument.";
-            var spacerLength = 2;
-            var firstColumnWidth = 18;
-            var consoleWidth = 40; // Super skinny console.
-            var paddedNewline = $"\n{new string(' ', firstColumnWidth + spacerLength)}";
-
             var expected = "This argument" +
-                paddedNewline + "description is" +
-                paddedNewline + "really long. It is a" +
-                paddedNewline + "great argument. The" +
-                paddedNewline + "best argument.";
+                _paddedNewline + "description is" +
+                _paddedNewline + "really long. It is a" +
+                _paddedNewline + "great argument. The" +
+                _paddedNewline + "best argument.";
 
-            var subject = new DescriptionFormatter(firstColumnWidth, spacerLength, consoleWidth: consoleWidth);
+            AssertExpected(originalText, expected);
+        }
 
-            var result = subject.Wrap(originalText);
+        [Fact]
+        public void LongRunningFirstLineStaysOnFirstLine()
+        {
+            var originalText = "SomeReallyLongWordWithNoSpacesAtAll the end.";
+            var expected = "SomeReallyLongWordWithNoSpacesAtAll" +
+                _paddedNewline + "the end.";
 
-            result.Should().Be(expected);
+            AssertExpected(originalText, expected);
+        }
+
+        [Theory]
+        [InlineData("I want\nmy own newlines\nplease the end.\nBut long lines do still get broken up correctly!")]
+        [InlineData("I want\r\nmy own newlines\r\nplease the end.\r\nBut long lines do still get broken up correctly!")]
+        public void ExplicitNewLinesArePreservedRegardlessOfType(string originalText)
+        {
+            var expected = "I want" +
+                _paddedNewline + "my own newlines" +
+                _paddedNewline + "please the end." +
+                _paddedNewline + "But long lines do" +
+                _paddedNewline + "still get broken up" +
+                _paddedNewline + "correctly!";
+
+            AssertExpected(originalText, expected);
+        }
+
+        [Theory]
+        [InlineData("You can have\n\ndouble lines.")]
+        [InlineData("You can have\r\n\r\ndouble lines.")]
+        public void DoubleLinesArePreserved(string originalText)
+        {
+            var expected = "You can have" +
+                _paddedNewline +
+                _paddedNewline + "double lines.";
+
+            AssertExpected(originalText, expected);
         }
     }
 }
