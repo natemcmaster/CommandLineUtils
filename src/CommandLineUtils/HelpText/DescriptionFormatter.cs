a467b0af402dd08159712ac11ef37ce3fd640bbf
Kyle Rader <kyrader@microsoft.com>
2019-06-27 11:07:36 -0700

    Add failing test for DescriptionFormatter

---
+++ src/CommandLineUtils/HelpText/DescriptionFormatter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace McMaster.Extensions.CommandLineUtils.HelpText
+{
+    /// <summary>
+    /// A formatter for creating nicely wrapped descriptions for display on the command line in the second column
+    /// of generated help text.
+    /// </summary>
+    internal class DescriptionFormatter
+    {
+        private int _firstColumnWidth;
+        private int _spacerLength;
+        private int _consoleWidth;
+
+        public DescriptionFormatter(int firstColumnWidth, int spacerLength, int? consoleWidth = null)
+        {
+            _firstColumnWidth = firstColumnWidth;
+            _spacerLength = spacerLength;
+            _consoleWidth = consoleWidth ?? Console.BufferWidth;
+        }
+
+        public string Wrap(string original)
+        {
+            return "";
+        }
+    }
+}
---
+++ test/CommandLineUtils.Tests/DescriptionFormatterTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using McMaster.Extensions.CommandLineUtils.HelpText;
+using Xunit;
+
+namespace McMaster.Extensions.CommandLineUtils.Tests
+{
+    public class DescriptionFormatterTests
+    {
+        [Fact]
+        public void ItWrapsADescriptionBasedOnConsoleWidthAndFirstColumnSize()
+        {
+            var originalText = "This argument description is really long. It is a great argument. The best argument.";
+            var spacerLength = 2;
+            var firstColumnWidth = 18;
+            var consoleWidth = 40; // Super skinny console.
+            var paddedNewline = $"\n{new string(' ', firstColumnWidth + spacerLength)}";
+
+            var expected = "This argument" +
+                paddedNewline + "description is" +
+                paddedNewline + "really long. It is a" +
+                paddedNewline + "great argument. The" +
+                paddedNewline + "best argument.";
+
+            var subject = new DescriptionFormatter(firstColumnWidth, spacerLength, consoleWidth: consoleWidth);
+
+            var result = subject.Wrap(originalText);
+
+            result.Should().Be(expected);
+        }
+    }
+}
