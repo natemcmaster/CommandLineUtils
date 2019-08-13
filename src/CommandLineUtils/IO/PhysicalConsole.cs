c15cabbf994e9e7073f1a5024e2cacb358f2d98e
Nate McMaster <nate.mcmaster@gmail.com>
2019-08-12 22:12:27 -0700

    fixup! Add add all api to shipped.txt

--- src/CommandLineUtils/IO/PhysicalConsole.cs
+++ src/CommandLineUtils/IO/PhysicalConsole.cs
@@ -17,6 +17,8 @@ namespace McMaster.Extensions.CommandLineUtils
         /// </summary>
         public static IConsole Singleton { get; } = new PhysicalConsole();
 
+        // TODO: in 3.0 make this type truly a singleton by adding a private ctor
+        // this is techinally a breaking change, so wait till 3.0
         // private PhysicalConsole()
         // {
         // }
