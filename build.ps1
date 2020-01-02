61072e4944cbdbe0a78a248b9b9677de3affd9dc
Nate McMaster <nate.mcmaster@gmail.com>
2020-01-01 23:06:48 -0800

    infra: add Coverlet for code coverage in tests (#328)

--- build.ps1
+++ build.ps1
@@ -42,7 +42,6 @@ if (-not (Test-Path variable:\IsCoreCLR)) {
 $CodeSign = $sign -or ($ci -and -not $isPr -and $IsWindows)
 
 if ($CodeSign) {
-    exec dotnet tool restore
     $MSBuildArgs += '-p:CodeSign=true'
 }
 
@@ -50,6 +49,7 @@ $artifacts = "$PSScriptRoot/artifacts/"
 
 Remove-Item -Recurse $artifacts -ErrorAction Ignore
 
+exec dotnet tool restore
 exec dotnet build --configuration $Configuration '-warnaserror:CS1591' @MSBuildArgs
 exec dotnet pack --no-restore --no-build --configuration $Configuration -o $artifacts @MSBuildArgs
 exec dotnet build --configuration $Configuration "$PSScriptRoot/docs/samples/samples.sln"
@@ -63,7 +63,15 @@ if ($env:TF_BUILD) {
 }
 
 exec dotnet test --no-restore --no-build --configuration $Configuration `
+    --collect:"XPlat Code Coverage" `
     @testArgs `
     @MSBuildArgs
+
+if ($ci) {
+    exec dotnet tool run reportgenerator `
+        "-reports:$PSScriptRoot/**/coverage.cobertura.xml" `
+        "-targetdir:$PSScriptRoot/coverlet/reports" `
+        "-reporttypes:Cobertura"
+}
 
 write-host -f magenta 'Done'
