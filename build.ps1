809960c12d91b2fc96fdeb1e2c3c2b8061f15f67
Nate McMaster <nate.mcmaster@gmail.com>
2021-01-30 23:01:40 -0800

    cleanup: simplify CI settings

--- build.ps1
+++ build.ps1
@@ -4,9 +4,7 @@ param(
     [ValidateSet('Debug', 'Release')]
     $Configuration = $null,
     [switch]
-    $ci,
-    [Parameter(ValueFromRemainingArguments = $true)]
-    [string[]]$MSBuildArgs
+    $ci
 )
 
 Set-StrictMode -Version 1
@@ -14,23 +12,8 @@ $ErrorActionPreference = 'Stop'
 
 Import-Module -Force -Scope Local "$PSScriptRoot/src/common.psm1"
 
-#
-# Main
-#
-
-$isPr = $env:BUILD_REASON -eq 'PullRequest'
-
-if ($env:CI -eq 'true') {
-    $ci = $true
-    & dotnet --info
-}
-
 if (!$Configuration) {
     $Configuration = if ($ci) { 'Release' } else { 'Debug' }
-}
-
-if ($ci) {
-    $MSBuildArgs += '-p:CI=true'
 }
 
 if (-not (Test-Path variable:\IsCoreCLR)) {
@@ -50,28 +33,17 @@ if ($ci) {
 
 exec dotnet tool run dotnet-format -- -v detailed @formatArgs "$PSScriptRoot/CommandLineUtils.sln"
 exec dotnet tool run dotnet-format -- -v detailed @formatArgs "$PSScriptRoot/docs/samples/samples.sln"
-exec dotnet build --configuration $Configuration '-warnaserror:CS1591' @MSBuildArgs
-exec dotnet pack --no-restore --no-build --configuration $Configuration -o $artifacts @MSBuildArgs
+exec dotnet build --configuration $Configuration '-warnaserror:CS1591'
+exec dotnet pack --no-build --configuration $Configuration -o $artifacts
 exec dotnet build --configuration $Configuration "$PSScriptRoot/docs/samples/samples.sln"
 
 [string[]] $testArgs=@()
 if (-not $IsWindows) {
     $testArgs += '-p:TestFullFramework=false'
 }
-if ($env:TF_BUILD) {
-    $testArgs += '--logger', 'trx'
-}
 
-exec dotnet test --no-restore --no-build --configuration $Configuration `
+exec dotnet test --no-build --configuration $Configuration `
     --collect:"XPlat Code Coverage" `
-    @testArgs `
-    @MSBuildArgs
-
-if ($ci) {
-    exec dotnet tool run reportgenerator `
-        "-reports:$PSScriptRoot/**/coverage.cobertura.xml" `
-        "-targetdir:$PSScriptRoot/coverlet/reports" `
-        "-reporttypes:Cobertura"
-}
+    @testArgs
 
 write-host -f green 'BUILD SUCCEEDED'
